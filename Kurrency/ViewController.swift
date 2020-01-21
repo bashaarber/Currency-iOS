38efe3701482230852f5a39ead1e1be952989b76
arberzs <arber@zombiesoup.com>
2020-01-21 21:56:08 +0100

    Initial Commit

---
+++ Kurrency/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Kurrency
+//
+//  Created by Arber Basha on 21.1.20.
+//  Copyright © 2020 Arber Basha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KurrencyTests/KurrencyTests.swift
@@ -0,0 +1,34 @@
+//
+//  KurrencyTests.swift
+//  KurrencyTests
+//
+//  Created by Arber Basha on 21.1.20.
+//  Copyright © 2020 Arber Basha. All rights reserved.
+//
+
+import XCTest
+@testable import Kurrency
+
+class KurrencyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
